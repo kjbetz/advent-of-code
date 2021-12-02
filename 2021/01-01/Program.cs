ef313d721802f8ca87fbc0131b7ad52c3f653197
Kristopher Betz <kbetz@e3sentinel.com>
2021-12-01 23:45:07 -0800

    Day 1

---
+++ 2021/01-01/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int counter = 0;
+int hold = 0;
+
+foreach (string line in File.ReadLines(@"input.txt"))
+{
+    var depth = Int32.Parse(line.Trim());
+
+    if (hold != 0)
+    {
+        if (depth > hold)
+        {
+            counter++;
+        }
+
+        hold = depth;
+    }
+    else
+    {
+        hold = depth;
+    }
+}
+
+Console.WriteLine(counter);
---
+++ 2021/01-02/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int counter = 0;
+List<int> depths = new();
+
+foreach (string line in File.ReadLines(@"input.txt"))
+{
+    int depth = Int32.Parse(line);
+
+    depths.Add(depth);
+
+    if (depths.Count() > 4) depths.RemoveAt(0);
+
+    if (depths.Count() == 4)
+    {
+        if (depths.GetRange(1, 3).Sum() > depths.GetRange(0, 3).Sum())
+        {
+            counter++;
+        }
+    }
+}
+
+Console.WriteLine(counter);
