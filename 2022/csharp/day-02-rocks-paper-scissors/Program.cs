0a3b88c02206fdfa130a9af013a0e715ba7ced54
Kristopher Betz <kbetz@avantusfederal.com>
2022-12-02 10:35:54 -0800

    Day 2

--- 2022/csharp/day-02-rocks-paper-scissors/Program.cs
+++ 2022/csharp/day-02-rocks-paper-scissors/Program.cs
@@ -1,39 +1,72 @@
 ﻿string[] lines = System.IO.File.ReadAllLines(@"./input.txt");
 
-int score = 0;
+// Console.WriteLine($"Your score is: {OriginalAttempt(lines)}");
+Console.WriteLine($"Your score is: {SecondAttempt(lines)}");
 
-foreach (string line in lines)
+int SecondAttempt(string[] lines)
 {
-    (string hand1, string outcome) = line.Split(" ");
+    Dictionary<string, int> lookup = new();
 
-    score += hand1 switch
+    lookup.Add("A X", 3);
+    lookup.Add("A Y", 4);
+    lookup.Add("A Z", 8);
+    lookup.Add("B X", 1);
+    lookup.Add("B Y", 5);
+    lookup.Add("B Z", 9);
+    lookup.Add("C X", 2);
+    lookup.Add("C Y", 6);
+    lookup.Add("C Z", 7);
+
+    int score = 0;
+
+    foreach (string line in lines)
     {
-        "A" => outcome switch
-        {
-            "X" => 3,
-            "Y" => 4,
-            "Z" => 8,
-            _ => 0,
-        },
-        "B" => outcome switch
-        {
-            "X" => 1,
-            "Y" => 5,
-            "Z" => 9,
-            _ => 0,
-        },
-        "C" => outcome switch
-        {
-            "X" => 2,
-            "Y" => 6,
-            "Z" => 7,
-            _ => 0,
-        },
-        _ => 0,
-    };
+        score += lookup[line];
+    }
+
+    return score;
 }
 
-Console.WriteLine($"Your score is: {score}");
+int OriginalAttempt(string[] lines)
+{
+    int score = 0;
+
+    foreach (string line in lines)
+    {
+        (string hand1, string outcome) = line.Split(" ");
+
+        score += hand1 switch
+        {
+            "A" => outcome switch
+            {
+                "X" => 3,
+                "Y" => 4,
+                "Z" => 8,
+                _ => 0,
+            },
+            "B" => outcome switch
+            {
+                "X" => 1,
+                "Y" => 5,
+                "Z" => 9,
+                _ => 0,
+            },
+            "C" => outcome switch
+            {
+                "X" => 2,
+                "Y" => 6,
+                "Z" => 7,
+                _ => 0,
+            },
+            _ => 0,
+        };
+    }
+
+    return score;
+}
+
+
+
 
 public static class Extensions
 {
@@ -41,5 +74,13 @@ public static class Extensions
     {
         string1 = array[0];
         string2 = array[1];
+    }
+
+    // Was starting to build dictionary with tuple of (string hand, string outcome)
+    // Not needed, don't know what I was thinking. Of course I can just add
+    // the line string "A Z"
+    public static void Deconstruct(this string[] array, out (string, string) match)
+    {
+        match = (array[0], array[1]);
     }
 }
---
+++ 2022/csharp/day-03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] lines = System.IO.File.ReadAllLines(@"./input.txt");
