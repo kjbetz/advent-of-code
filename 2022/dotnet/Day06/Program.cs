31a4d00ffcb5851ea288c8e0b94155b3b47dbc30
Kristopher Betz <kjbetz@gmail.com>
2022-12-05 22:11:48 -0800

    Day 6 complete

---
+++ 2022/dotnet/Day06/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] lines = File.ReadAllLines("./input.txt");
+
+foreach (string line in lines)
+{
+    Console.WriteLine($"{FindSequenceNumber(line)}");
+}
+
+int FindSequenceNumber(string line)
+{
+    int left = 0;
+    int right = 0;
+    Dictionary<char, int> code = new();
+
+    while (left < line.Length && right < line.Length)
+    {
+        if (!code.TryAdd(line[right], right))
+        {
+            code[line[right]] = right;
+
+            while (true)
+            {
+                if (line[left] == line[right])
+                {
+                    left++;
+                    break;
+                } else {
+                    code.Remove(line[left]);
+                    left++;
+                }
+            }
+        }
+
+        if (right - left == 13)
+        {
+            return right + 1;
+        }
+
+        right++;
+    }
+
+    return 0;
+}
+
