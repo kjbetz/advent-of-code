d93cfcf99f9895629f4b9393e2aed35281763663
Kristopher Betz <kjbetz@gmail.com>
2023-12-06 22:19:55 -0800

    Day 4 complete

--- 2023/dotnet/03/Program.cs
+++ 2023/dotnet/03/Program.cs
@@ -1,277 +1,243 @@
-﻿public class Program
+﻿string[] lines = File.ReadAllLines(@"input.txt");
+List<int> partNumbers = new();
+
+HashSet<Position> positions = new();
+Dictionary<Position, Gears> gears = new();
+
+for (int y = 0; y < lines.Length; y++)
 {
-    public record Position(int X, int Y);
+    string tempNumber = string.Empty;
+    bool isValidPartNumber = false;
 
-    public record Gears(int Gear1, int Gear2);
-
-    public static void Main()
+    for (int x = 0; x < lines[y].Length; x++)
     {
-        Console.WriteLine("Hello World!");
+        char c = lines[y][x];
 
-        string[] lines = File.ReadAllLines(@"input.txt");
-        List<int> partNumbers = new();
-
-        HashSet<Position> positions = new();
-        Dictionary<Position, Gears> gears = new();
-
-        for (int y = 0; y < lines.Length; y++)
+        if (char.IsDigit(c))
         {
-            string tempNumber = string.Empty;
-            bool isValidPartNumber = false;
+            tempNumber += c;
 
-            for (int x = 0; x < lines[y].Length; x++)
+            // Check position above
+            if (y > 0)
             {
-                char c = lines[y][x];
-
-                if (char.IsDigit(c))
+                if (!char.IsDigit(lines[y - 1][x]) && lines[y - 1][x] != '.')
                 {
-                    tempNumber += c;
-
-                    // Check position above
-                    if (y > 0)
-                    {
-                        if (!char.IsDigit(lines[y - 1][x]) && lines[y - 1][x] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position below
-                    if (y < lines.Length - 1)
-                    {
-                        if (!char.IsDigit(lines[y + 1][x]) && lines[y + 1][x] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position to the left
-                    if (x > 0)
-                    {
-                        if (!char.IsDigit(lines[y][x - 1]) && lines[y][x - 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position to the right
-                    if (x < lines[y].Length - 1)
-                    {
-                        if (!char.IsDigit(lines[y][x + 1]) && lines[y][x + 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position above and to the left
-                    if (y > 0 && x > 0)
-                    {
-                        if (!char.IsDigit(lines[y - 1][x - 1]) && lines[y - 1][x - 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position above and to the right
-                    if (y > 0 && x < lines[y].Length - 1)
-                    {
-                        if (!char.IsDigit(lines[y - 1][x + 1]) && lines[y - 1][x + 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position below and to the left
-                    if (y < lines.Length - 1 && x > 0)
-                    {
-                        if (!char.IsDigit(lines[y + 1][x - 1]) && lines[y + 1][x - 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-
-                    // Check position below and to the right
-                    if (y < lines.Length - 1 && x < lines[y].Length - 1)
-                    {
-                        if (!char.IsDigit(lines[y + 1][x + 1]) && lines[y + 1][x + 1] != '.')
-                        {
-                            isValidPartNumber = true;
-                        }
-                    }
-                }
-                else
-                {
-                    if (tempNumber != string.Empty && isValidPartNumber)
-                    {
-                        partNumbers.Add(int.Parse(tempNumber));
-                    }
-
-                    tempNumber = string.Empty;
-                    isValidPartNumber = false;
+                    isValidPartNumber = true;
                 }
             }
 
+            // Check position below
+            if (y < lines.Length - 1)
+            {
+                if (!char.IsDigit(lines[y + 1][x]) && lines[y + 1][x] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position to the left
+            if (x > 0)
+            {
+                if (!char.IsDigit(lines[y][x - 1]) && lines[y][x - 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position to the right
+            if (x < lines[y].Length - 1)
+            {
+                if (!char.IsDigit(lines[y][x + 1]) && lines[y][x + 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position above and to the left
+            if (y > 0 && x > 0)
+            {
+                if (!char.IsDigit(lines[y - 1][x - 1]) && lines[y - 1][x - 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position above and to the right
+            if (y > 0 && x < lines[y].Length - 1)
+            {
+                if (!char.IsDigit(lines[y - 1][x + 1]) && lines[y - 1][x + 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position below and to the left
+            if (y < lines.Length - 1 && x > 0)
+            {
+                if (!char.IsDigit(lines[y + 1][x - 1]) && lines[y + 1][x - 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+
+            // Check position below and to the right
+            if (y < lines.Length - 1 && x < lines[y].Length - 1)
+            {
+                if (!char.IsDigit(lines[y + 1][x + 1]) && lines[y + 1][x + 1] != '.')
+                {
+                    isValidPartNumber = true;
+                }
+            }
+        }
+        else
+        {
             if (tempNumber != string.Empty && isValidPartNumber)
             {
                 partNumbers.Add(int.Parse(tempNumber));
             }
+
+            tempNumber = string.Empty;
+            isValidPartNumber = false;
         }
+    }
 
-        /*
-        foreach (int partNumber in partNumbers)
+    if (tempNumber != string.Empty && isValidPartNumber)
+    {
+        partNumbers.Add(int.Parse(tempNumber));
+    }
+}
+
+/*
+foreach (int partNumber in partNumbers)
+{
+    Console.WriteLine(partNumber);
+}
+*/
+
+// Console.WriteLine($"Number of part numbers: {partNumbers.Count}");
+Console.WriteLine($"Sum of part numbers: {partNumbers.Sum()}");
+
+// Part 2
+
+for (int y = 0; y < lines.Length; y++)
+{
+    string tempNumber = string.Empty;
+    bool isGear = false;
+
+    for (int x = 0; x < lines[y].Length; x++)
+    {
+        char c = lines[y][x];
+
+        if (char.IsDigit(c))
         {
-            Console.WriteLine(partNumber);
-        }
-        */
+            tempNumber += c;
 
-        // Console.WriteLine($"Number of part numbers: {partNumbers.Count}");
-        Console.WriteLine($"Sum of part numbers: {partNumbers.Sum()}");
-
-        // Part 2
-
-        for (int y = 0; y < lines.Length; y++)
-        {
-            string tempNumber = string.Empty;
-            bool isGear = false;
-
-            for (int x = 0; x < lines[y].Length; x++)
+            // Check position above
+            if (y > 0)
             {
-                char c = lines[y][x];
-
-                if (char.IsDigit(c))
+                if (lines[y - 1][x] == '*')
                 {
-                    tempNumber += c;
-
-                    // Check position above
-                    if (y > 0)
+                    isGear = true;
+                    if (!positions.Contains(new Position(x, y - 1)))
                     {
-                        if (lines[y - 1][x] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x, y - 1)))
-                            {
-                                positions.Add(new Position(x, y - 1));
-                            }
-                        }
+                        positions.Add(new Position(x, y - 1));
                     }
-
-                    // Check position below
-                    if (y < lines.Length - 1)
-                    {
-                        if (lines[y + 1][x] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x, y + 1)))
-                            {
-                                positions.Add(new Position(x, y + 1));
-                            }
-                        }
-                    }
-
-                    // Check position to the left
-                    if (x > 0)
-                    {
-                        if (lines[y][x - 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x - 1, y)))
-                            {
-                                positions.Add(new Position(x - 1, y));
-                            }
-                        }
-                    }
-
-                    // Check position to the right
-                    if (x < lines[y].Length - 1)
-                    {
-                        if (lines[y][x + 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x + 1, y)))
-                            {
-                                positions.Add(new Position(x + 1, y));
-                            }
-                        }
-                    }
-
-                    // Check position above and to the left
-                    if (y > 0 && x > 0)
-                    {
-                        if (lines[y - 1][x - 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x - 1, y - 1)))
-                            {
-                                positions.Add(new Position(x - 1, y - 1));
-                            }
-                        }
-                    }
-
-                    // Check position above and to the right
-                    if (y > 0 && x < lines[y].Length - 1)
-                    {
-                        if (lines[y - 1][x + 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x + 1, y - 1)))
-                            {
-                                positions.Add(new Position(x + 1, y - 1));
-                            }
-                        }
-                    }
-
-                    // Check position below and to the left
-                    if (y < lines.Length - 1 && x > 0)
-                    {
-                        if (lines[y + 1][x - 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x - 1, y + 1)))
-                            {
-                                positions.Add(new Position(x - 1, y + 1));
-                            }
-                        }
-                    }
-
-                    // Check position below and to the right
-                    if (y < lines.Length - 1 && x < lines[y].Length - 1)
-                    {
-                        if (lines[y + 1][x + 1] == '*')
-                        {
-                            isGear = true;
-                            if (!positions.Contains(new Position(x + 1, y + 1)))
-                            {
-                                positions.Add(new Position(x + 1, y + 1));
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (tempNumber != string.Empty && isGear)
-                    {
-                        foreach (Position position in positions)
-                        {
-                            if (gears.ContainsKey(position))
-                            {
-                                gears[position] = new Gears(
-                                    gears[position].Gear1,
-                                    int.Parse(tempNumber)
-                                );
-                            }
-                            else
-                            {
-                                gears.Add(position, new Gears(int.Parse(tempNumber), 0));
-                            }
-                        }
-                    }
-
-                    tempNumber = string.Empty;
-                    isGear = false;
-                    positions.Clear();
                 }
             }
 
+            // Check position below
+            if (y < lines.Length - 1)
+            {
+                if (lines[y + 1][x] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x, y + 1)))
+                    {
+                        positions.Add(new Position(x, y + 1));
+                    }
+                }
+            }
+
+            // Check position to the left
+            if (x > 0)
+            {
+                if (lines[y][x - 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x - 1, y)))
+                    {
+                        positions.Add(new Position(x - 1, y));
+                    }
+                }
+            }
+
+            // Check position to the right
+            if (x < lines[y].Length - 1)
+            {
+                if (lines[y][x + 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x + 1, y)))
+                    {
+                        positions.Add(new Position(x + 1, y));
+                    }
+                }
+            }
+
+            // Check position above and to the left
+            if (y > 0 && x > 0)
+            {
+                if (lines[y - 1][x - 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x - 1, y - 1)))
+                    {
+                        positions.Add(new Position(x - 1, y - 1));
+                    }
+                }
+            }
+
+            // Check position above and to the right
+            if (y > 0 && x < lines[y].Length - 1)
+            {
+                if (lines[y - 1][x + 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x + 1, y - 1)))
+                    {
+                        positions.Add(new Position(x + 1, y - 1));
+                    }
+                }
+            }
+
+            // Check position below and to the left
+            if (y < lines.Length - 1 && x > 0)
+            {
+                if (lines[y + 1][x - 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x - 1, y + 1)))
+                    {
+                        positions.Add(new Position(x - 1, y + 1));
+                    }
+                }
+            }
+
+            // Check position below and to the right
+            if (y < lines.Length - 1 && x < lines[y].Length - 1)
+            {
+                if (lines[y + 1][x + 1] == '*')
+                {
+                    isGear = true;
+                    if (!positions.Contains(new Position(x + 1, y + 1)))
+                    {
+                        positions.Add(new Position(x + 1, y + 1));
+                    }
+                }
+            }
+        }
+        else
+        {
             if (tempNumber != string.Empty && isGear)
             {
                 foreach (Position position in positions)
@@ -286,15 +252,38 @@
                     }
                 }
             }
+
+            tempNumber = string.Empty;
+            isGear = false;
+            positions.Clear();
         }
+    }
 
-        List<int> gearRatios = new();
-
-        foreach (Gears gear in gears.Values)
+    if (tempNumber != string.Empty && isGear)
+    {
+        foreach (Position position in positions)
         {
-            gearRatios.Add(gear.Gear1 * gear.Gear2);
+            if (gears.ContainsKey(position))
+            {
+                gears[position] = new Gears(gears[position].Gear1, int.Parse(tempNumber));
+            }
+            else
+            {
+                gears.Add(position, new Gears(int.Parse(tempNumber), 0));
+            }
         }
-
-        Console.WriteLine($"Sum of gear ratios: {gearRatios.Sum()}");
     }
 }
+
+List<int> gearRatios = new();
+
+foreach (Gears gear in gears.Values)
+{
+    gearRatios.Add(gear.Gear1 * gear.Gear2);
+}
+
+Console.WriteLine($"Sum of gear ratios: {gearRatios.Sum()}");
+
+public record Position(int X, int Y);
+
+public record Gears(int Gear1, int Gear2);
---
+++ 2023/dotnet/04/Program.cs
@@ -0,0 +1,95 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+List<int> points = new();
+
+foreach (string line in lines)
+{
+    string[] game = line.Split(':');
+    string[] numbers = game[1].Trim().Split('|');
+    string[] winningNumbers = numbers[0].Trim().Split(' ');
+    string[] yourNumbers = numbers[1].Trim().Split(' ');
+
+    int[] winningNumbersInt = winningNumbers
+        .Where(x => x.Trim().Length > 0)
+        .Select(x => int.Parse(x.ToString().Trim()))
+        .ToArray();
+    int[] yourNumbersInt = yourNumbers
+        .Where(x => x.Trim().Length > 0)
+        .Select(x => int.Parse(x.ToString().Trim()))
+        .ToArray();
+
+    int matches = winningNumbersInt.Intersect(yourNumbersInt).Count();
+
+    int pointsForGame = 0;
+    string math = $"{pointsForGame}";
+
+    for (int i = 0; i < matches; i++)
+    {
+        pointsForGame += pointsForGame == 0 ? 1 : pointsForGame;
+    }
+
+    if (pointsForGame > 0)
+    {
+        points.Add(pointsForGame);
+    }
+}
+
+Console.WriteLine($"Part 1 points: {points.Sum()}");
+
+// Part 2
+Dictionary<int, Game> games = new();
+
+foreach (string line in lines)
+{
+    string[] game = line.Split(':');
+    string[] gameName = game[0].Trim().Split();
+    int gameId = int.Parse(gameName[gameName.Length - 1].Trim());
+
+    string[] numbers = game[1].Trim().Split('|');
+    string[] winningNumbers = numbers[0].Trim().Split();
+    string[] yourNumbers = numbers[1].Trim().Split();
+
+    int[] winningNumbersInt = winningNumbers
+        .Where(x => x.Trim().Length > 0)
+        .Select(x => int.Parse(x.ToString().Trim()))
+        .ToArray();
+
+    int[] yourNumbersInt = yourNumbers
+        .Where(x => x.Trim().Length > 0)
+        .Select(x => int.Parse(x.ToString().Trim()))
+        .ToArray();
+
+    int matches = winningNumbersInt.Intersect(yourNumbersInt).Count();
+
+    games.Add(
+        gameId,
+        new Game
+        {
+            Id = gameId,
+            WinningNumbers = winningNumbersInt,
+            YourNumbers = yourNumbersInt,
+            Matches = matches
+        }
+    );
+}
+
+foreach (Game scratchcard in games.Values)
+{
+    if (scratchcard.Matches > 0)
+    {
+        for (int i = 0; i < scratchcard.Matches; i++)
+        {
+            games[scratchcard.Id + 1 + i].Count += 1 * scratchcard.Count;
+        }
+    }
+}
+
+Console.WriteLine($"Part 2 total scratchcards: {games.Values.Sum(g => g.Count)}");
+
+public class Game
+{
+    public int Id { get; set; }
+    public int[] WinningNumbers { get; set; }
+    public int[] YourNumbers { get; set; }
+    public int Matches { get; set; }
+    public int Count { get; set; } = 1;
+}
