1ae17e66abaac69ccf32ff8a4a675cd9e1cccc76
Kristopher Betz <kjbetz@gmail.com>
2024-12-01 10:05:23 -0800

    2025 Day One

--- 2023/dotnet/10/Program.cs
+++ 2023/dotnet/10/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("test1-input.txt");
+
+Coordinate? startingCoordinate = null;
+
+for (int y = 0; y < lines.Length; y++)
+{
+    for (int x = 0; x < lines[y].Length; x++)
+    {
+        if (lines[y][x] == 'S')
+        {
+            startingCoordinate = new Coordinate(x, y);
+            break;
+        }
+    }
+
+    if (startingCoordinate != null)
+        break;
+}
+Dictionary<Coordinate, int> alreadyVisited = new();
+Walk(startingCoordinate, alreadyVisited);
+
+Console.WriteLine($"Starting coordinate: ({startingCoordinate?.X}, {startingCoordinate?.Y})");
+
+void Walk(Coordinate currentCoordinate, Dictionary<Coordinate, int> alreadyVisited)
+{
+    if (
+        currentCoordinate.X < 0
+        || currentCoordinate.X > lines[0].Length
+        || currentCoordinate.Y < 0
+        || currentCoordinate.Y > lines.Length
+    )
+        return;
+
+    
+}
+
+record Coordinate(int X, int Y);
---
+++ 2024/01-historian-hysteria/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+List<string> listA = [];
+List<string> listB = [];
+int sum = 0;
+int sum2 = 0;
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split("   ");
+    listA.Add(parts[0]);
+    listB.Add(parts[1]);
+}
+
+// Part One
+listA.Sort();
+listB.Sort();
+
+for (int i = 0; i < listA.Count; i++)
+    if (int.Parse(listA[i]) > int.Parse(listB[i]))
+        sum += int.Parse(listA[i]) - int.Parse(listB[i]);
+    else
+        sum += int.Parse(listB[i]) - int.Parse(listA[i]);
+
+Console.WriteLine(sum);
+
+
+// Part Two
+Dictionary<string, int> listBOccurrences = new();
+
+foreach (string item in listB)
+    if (listBOccurrences.ContainsKey(item))
+        listBOccurrences[item]++;
+    else
+        listBOccurrences[item] = 1;
+
+foreach (string item in listA)
+    if (listBOccurrences.ContainsKey(item))
+        sum2 += int.Parse(item) * listBOccurrences[item];
+
+Console.WriteLine(sum2);
